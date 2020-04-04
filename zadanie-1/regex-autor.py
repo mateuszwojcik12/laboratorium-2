8cfe0d9ea86f100742f33bf6ffeca9dd863424d1
aga4227 <agnieszka_podgorna@outlook.com>
2020-04-04 14:38:04 +0200

    Odmiana slowo1

--- zadanie-1/regex-autor.py
+++ zadanie-1/regex-autor.py
@@ -3,8 +3,10 @@ import re as re
 def function_check_regex():
     autor1 = 'Agnieszka'
     autor2 = 'Mateusz'
-    print(re.search(r"Ag[aęąi]|Agnieszk[aęą]", autor1))
+    print(re.search(r"Ag[aęąio]|Adze|Agnieszk[aoięą]|Agnieszc[e]", autor1))
     print(re.search(r"Mateus.", autor2))
 
 if __name__ == '__main__':
     function_check_regex()
+
+
